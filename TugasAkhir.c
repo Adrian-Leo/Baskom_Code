bdd38df3a663384ea179049527e94de33f1447a2
leo <leopradanaadrian@ymail.com>
2022-05-24 11:16:52 +0700

    tugas updet

--- TugasAkhir.c
+++ TugasAkhir.c
@@ -51,14 +51,14 @@ void goFoodMenu(){
 //print go ride receipt
 void printGoRide(){
 
-		printf( "\t**********************************************\n" );
-        printf( "\t|                G O  R I D E                |\n" );
-        printf( "\t|                R E C E I P T               |\n" );
-        printf( "\t**********************************************\n" );
-		printf( "\n   	Alamat Asal\t	:   %s " , goride_1.alamat_asal );
-		printf( "\n   	Alamat Tujuan\t	:   %s" , goride_1.alamat_tujuan  );
-		printf( "\n    	Rentang Jarak\t	:   %0.2f Km " ,goride_1.jarak  );
-		printf( "\n    	Total Harga\t	: 	%0.2f Rupiah" ,goride_1.total );	
+	printf( "\t**********************************************\n" );
+    printf( "\t|                G O  R I D E                |\n" );
+    printf( "\t|                R E C E I P T               |\n" );
+    printf( "\t**********************************************\n" );
+	printf( "\n   	Alamat Asal\t	:   %s " , goride_1.alamat_asal );
+	printf( "\n   	Alamat Tujuan\t	:   %s" , goride_1.alamat_tujuan  );
+	printf( "\n    	Rentang Jarak\t	:   %0.2f Km " ,goride_1.jarak  );
+	printf( "\n    	Total Harga\t	: 	%0.2f Rupiah" ,goride_1.total );	
 		
 	
 }
@@ -66,14 +66,14 @@ void printGoRide(){
 //print go food receipt
 void printGoFood()
 {
-		printf( "\t**********************************************\n" );
-	    printf( "\t|                G O  F O O D	            |\n" );
-	    printf( "\t|                R E C E I P T               |\n" );
-	    printf( "\t**********************************************\n" );
-		printf( "\n Alamat Rumah\t 		: 	%s	" , gofood_1.alamat );
-		printf( "\n Nama Menu\t 		:	%s	" , gofood_1.nama_menu );
-		printf( "\n Jumlah Pesanan\t	: 	%d	" , gofood_1.jumlah );
-		printf( "\n Total Harga\t 		: Rp%0.2f Rupiah" ,gofood_1.total ) ;
+	printf( "\t**********************************************\n" );
+	printf( "\t|                G O  F O O D	            |\n" );
+	printf( "\t|                R E C E I P T               |\n" );
+	printf( "\t**********************************************\n" );
+	printf( "\n Alamat Rumah\t 		: 	%s	" , gofood_1.alamat );
+	printf( "\n Nama Menu\t 		:	%s	" , gofood_1.nama_menu );
+	printf( "\n Jumlah Pesanan\t	: 	%d	" , gofood_1.jumlah );
+	printf( "\n Total Harga\t 		: Rp%0.2f Rupiah" ,gofood_1.total ) ;
 	
 }
 
@@ -147,25 +147,25 @@ void go_foods(){
 //print transaction history
 void transactionHistory(){
 	system("cls");
-		int history;
-		printf( "\n||||||||||||||||||||||LIHAT RIWAYAT TRANSAKSI|||||||||||||||||||||||\n");
-		printf( "\n1. Go Ride " );
-		printf( "\n2. Go Food");
-		printf( "\n Choose Option : " );
+	int history;
+	printf( "\n||||||||||||||||||||||LIHAT RIWAYAT TRANSAKSI|||||||||||||||||||||||\n");
+	printf( "\n1. Go Ride " );
+	printf( "\n2. Go Food");
+	printf( "\n Choose Option : " );
 
-		scanf("%d",&history);
-		switch(history){
+	scanf("%d",&history);
+	switch(history){
 		
-			case 1 :
-				printGoRide(goride_1);
-				break;	
-			case 2 :
-				printGoFood(gofood_1);
-				break;
+		case 1 :
+			printGoRide(goride_1);
+			break;	
+		case 2 :
+			printGoFood(gofood_1);
+			break;
 
-			default :
-				printf("\n INVALID INPUT \n");
-				break;
+		default :
+			printf("\n INVALID INPUT \n");
+			break;
 		}
 }
 
